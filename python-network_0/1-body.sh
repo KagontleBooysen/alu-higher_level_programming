fd141f17ad46c7fb685bdcbeab211ae1145b6f8e
KagontleBooysen <k.booysen@alustudent.com>
2022-07-29 21:17:11 +0200

    Task1

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that shows the response body redirecting /route_1
-curl -Ls "$1"
+# takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -s "$1" -X GET -L 
