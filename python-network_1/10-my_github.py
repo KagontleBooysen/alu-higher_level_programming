80501664211a49ded2c1ae4d2dae607cec950514
KagontleBooysen <k.booysen@alustudent.com>
2022-08-01 12:33:38 -0700

    Task10

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""Sends a search request for a given string to the Star Wars API.
-Usage: ./9-starwars.py <search string>
-  - The search request is sent to the Star Wars API search people endpoint.
-"""
-import sys
-import requests
+"""Takes in Github credentials (username and password) and uses the Github API
+to display an id"""
 
 
 if __name__ == "__main__":
-    url = "https://swapi.co/api/people"
-    params = {"search": sys.argv[1]}
-    results = requests.get(url, params=params).json()
+    import requests
+    import sys
 
-    print("Number of results: {}".format(results.get("count")))
-    [print(r.get("name")) for r in results.get("results")]
+    r = requests.get('https://api.github.com/user',
+                     auth=(sys.argv[1], sys.argv[2]))
+    if r.status_code >= 400:
+        print('None')
+    else:
+        print(r.json().get('id'))]
