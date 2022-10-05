75dbc5cd2c901120065d04eb583110e5575087fd
KagontleBooysen <k.booysen@alustudent.com>
2022-10-05 09:28:45 -0700

    101-sorted.js

--- javascript_objects_scopes_closures/101-sorted.js
+++ javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
-let newDict = {};
+let const = {};
 for (let key in dict) {
   if (newDict[dict[key]] === undefined) {
     newDict[dict[key]] = [key];
   } else {
-    newDict[dict[key]].push(key);
+    newDict[dict[const]].push(const);
   }
 }
-console.log(newDict);;
+console.log(const);
