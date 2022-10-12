fe5f35c52006a9a97b7fa1a2ebdd2837bb8ee478
KagontleBooysen <k.booysen@alustudent.com>
2022-10-12 10:49:51 -0700

    101-starwars_characters.js

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -1,30 +1,79 @@
 #!/usr/bin/node
+/*
 const request = require('request');
-const url = 'http://swapi.co/api/films/';
-let id = parseInt(process.argv[2], 10);
-let characters = [];
-
-request(url, function (err, response, body) {
-  if (err == null) {
-    const resp = JSON.parse(body);
-    const results = resp.results;
-    if (id < 4) {
-      id += 3;
-    } else {
-      id -= 3;
-    }
-    for (let i = 0; i < results.length; i++) {
-      if (results[i].episode_id === id) {
-        characters = results[i].characters;
-        break;
-      }
-    }
-    for (let j = 0; j < characters.length; j++) {
-      request(characters[j], function (err, response, body) {
-        if (err == null) {
+const args = process.argv;
+let dicList = {};
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+request(`https://swapi-api.hbtn.io/api/films/${args[2]}`, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    const charEnd = data.characters;
+    const loopTime = charEnd.length;
+    for (let i = 0; i < loopTime; i++) {
+      request(charEnd[i], function (error, response, body) {
+        if (error) {
+          console.log(error);
+        } else {
           console.log(JSON.parse(body).name);
+          dicList[JSON.parse(body).name] = i;
         }
       });
+        sleep(8000);
+    }
+    console.log(dicList);
+  }
+});
+*/
+
+const request = require('request');
+const { argv, exit } = require('process');
+
+if (argv.length !== 3) {
+  exit(0);
+}
+
+const options = {
+  url: `https://swapi-api.hbtn.io/api/films/${argv[2]}/`,
+  method: 'GET',
+  headers: {
+    'Accept-Charset': 'utf-8'
+  }
+};
+
+function requestStart (optionsUrl, saveValue) {
+  request(optionsUrl, function (err, res, body) {
+    if (err) throw err;
+
+    const characters = JSON.parse(body).characters;
+    const result = {};
+
+    characters.forEach(element => {
+      request(element, function (err, res, _body) {
+        if (err) throw err;
+
+        const name = JSON.parse(_body).name;
+
+        result[element.split('/')[5]] = name;
+        saveValue(result, characters.length);
+      });
+    });
+  });
+}
+
+let i = 0;
+requestStart(options, function (dict, length) {
+  i++;
+  if (i === length) {
+    for (const name in dict) {
+      console.log(dict[name]);
     }
   }
 });
